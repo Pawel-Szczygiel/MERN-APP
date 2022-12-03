84379fe979cac36d222ad97459df0ed3c4985b28
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2022-12-03 14:39:55 +0100

    styling navigation

--- src/user/pages/User.jsx
+++ src/user/pages/User.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 
 const Users = () => {
+
+    const USERS = [
+        {
+            id: 1,
+            name:'pawel',
+            image: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561__340.png',
+            placeCount: '5'
+        }
+    ]
+
     return ( 
-        <h1>Lets start!</h1>
+        <UsersList items={USERS} />
      );
 }
  
